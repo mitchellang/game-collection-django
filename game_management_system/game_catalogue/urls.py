44b0ff7d6113091fde68d2352b65c43d29754afc
Mitchell Ang <m.ang@sms.ed.ac.uk>
2020-12-10 00:19:08 -0800

    last create game

--- game_management_system/game_catalogue/urls.py
+++ game_management_system/game_catalogue/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('games/', views.GameCollectionListView.as_view(), name = 'game-collection'),
-    path('games/<uuid:pk>', views.GameCollectionDetailView.as_view(), name = 'game-collection-detail'),
+    path('collection/', views.GameCollectionListView.as_view(), name = 'game-collection'),
+    path('collection/<uuid:pk>', views.GameCollectionDetailView.as_view(), name = 'game-collection-detail'),
 ]
 
 urlpatterns += [
@@ -13,7 +13,13 @@ urlpatterns += [
 
 # Add Remove collections
 urlpatterns += [
-    path('games/create/', views.GameCollectionCreate.as_view(), name='game-collection-create'),
-    path('games/<uuid:pk>/update/', views.GameCollectionUpdate.as_view(), name='game-collection-update'),
-    path('games/<uuid:pk>/delete/', views.GameCollectionDelete.as_view(), name='game-collection-delete'),
+    path('collection/create/', views.GameCollectionCreate.as_view(), name='game-collection-create'),
+    path('collection/<uuid:pk>/update/', views.GameCollectionUpdate.as_view(), name='game-collection-update'),
+    path('collection/<uuid:pk>/delete/', views.GameCollectionDelete.as_view(), name='game-collection-delete'),
+    ]
+
+urlpatterns += [
+    path('collection/<uuid:pk>/create/', views.GameCreate.as_view(), name='game-create'),
+    path('collection/<uuid:pk>/update/', views.GameUpdate.as_view(), name='game-update'),
+    path('collection/<uuid:pk>/delete/', views.GameDelete.as_view(), name='game-delete'),
     ]
--- game_management_system/game_catalogue/views.py
+++ game_management_system/game_catalogue/views.py
@@ -7,8 +7,10 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/accounts/login/')
 def index(request):
     """View function for home page of site."""
 
@@ -56,10 +58,31 @@ class GameCollectionCreate(CreateView):
 
 class GameCollectionUpdate(UpdateView):
     model = GameCollection
-    fields = '__all__'  # Not recommended (potential security issue if more fields added)
+    fields = ['collection_name', 'collection_description']  # Not recommended (potential security issue if more fields added)
 
 
 class GameCollectionDelete(DeleteView):
     model = GameCollection
     success_url = reverse_lazy('my-collection')
 
+
+class GameCreate(CreateView):
+    model = Game
+    fields = ['game_title', 'game_description','game_collection']
+
+    def form_valid(self, form):
+        owner = self.request.user
+        form.instance.owner = owner
+        form.instance.game_collection = Game.game_collection.filter(
+            game__game_collection__collection_id = self.request.resolver_match.kwargs.pk)
+        return super(GameCreate, self).form_valid(form)
+
+
+class GameUpdate(UpdateView):
+    model = GameCollection
+    fields = ['collection_name', 'collection_description']  # Not recommended (potential security issue if more fields added)
+
+
+class GameDelete(DeleteView):
+    model = GameCollection
+    success_url = reverse_lazy('my-collection')
